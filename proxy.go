89d8e5ef5e0854da89836e2f5912275112175c0d
ssab <qasw4554@gmail.com>
2019-03-24 19:10:41 +0900

    [add] ファイルの追加
    設定ファイルを書き換えるファイルと、それをテストする関数の追加

---
+++ proxy.go
@@ -0,0 +1,38 @@
+// プロキシファイルに#を加える関数と、抜き取る関数
+package main
+
+import (
+	"os"
+	"bufio"
+	"regexp"
+)
+
+// プロキシが書かれた行の先頭に#,コメントアウトを入れる(ファイルを書き換える)
+func ProxyAddComment(file *os.File) (err error) {
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		rep := regexp.MustCompile(`kanazawa-it.ac.jp`)
+		if rep.MatchString(line) {
+			line = rep.ReplaceAllString(line, "# kanazawa-it.ac.jp:8080")
+			//以下は、lineの行を書き換えたい
+			//~~~
+		}
+	}
+	return
+}
+
+// プロキシが書かれた行の先頭の#を抜く（実際には書き換える）
+func ProxySubComment(file *os.File) (err error) {
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		rep := regexp.MustCompile(`kanazawa-it.ac.jp`)
+		if rep.MatchString(line) {
+			line = rep.ReplaceAllString(line, "kanazawa-it.ac.jp:8080")
+			//以下で、ファイルの内容を書き換えたい
+			//~~~
+		}
+	}
+	return
+}
---
+++ proxy_test.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"regexp"
+	"testing"
+)
+
+func test_proxyAddComment(t *testing.T) {
+	//ファイルがあったら上書き、なかったら新規作成。最後に追記ではない
+	file, err := os.OpenFile("proxy.txt", os.O_WRONLY|os.O_CREATE, 0666)
+	if err != nil {
+		t.Error("Error to Open File", err)
+	}
+	defer file.Close()
+	fmt.Fprintln(file, "kanazawa-it.ac.jp:8080")
+
+	err := ProxyAddComment(file)
+	if err != nil {
+		t.Errorf("Error ProxyAddComment")
+	}
+
+	//以下で、正規表現をして、プロキシが書かれている行にコメントアウトができているかをテストする
+	scanner := bufio.Newcanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		//プロキシが書かれている行を見つけられるかを調べる
+		rex := regexp.MustCompile(`kanazawa-it.ac.jp:8080`)
+		//マッチ失敗時
+		if !rex.MatchString(line) {
+			t.Errorf("Error to regexp proxy")
+			return
+		}
+		//プロキシが記載されている行の先頭に、コメントアウトがされているかを調べる
+		coRex := regexp.MustCompile(`# kanazawa-it.ac.jp:8080`)
+		//マッチ失敗時
+		if !coRex.MatchString(line) {
+			t.Error("Error to write # to file", err)
+		}
+	}
+}
