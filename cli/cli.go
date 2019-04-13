2914c1484e763e3b71090846e0fe2af2f72cf61c
ssab <qasw4554@gmail.com>
2019-04-13 14:39:52 +0900

    [add]#13 考えうるすべてのサブコマンドの追加
    CLI化するためのすべてのサブコマンドを追加し、単体テスト完了。ただ、cli/cli.goのfpath変数に難あり。後ほど調べる。

--- cli/cli.go
+++ cli/cli.go
@@ -15,17 +15,41 @@ type cli struct {
 	outStream, errStream io.Writer
 }
 
+//var fpath string
+
 func (c *cli) Run(args []string) int {
 	//switch file proxy = swfpx
 	flags := flag.NewFlagSet("swfpx", flag.ContinueOnError)
 	flags.SetOutput(c.errStream)
-	var filepath string
-	flags.StringVar(&filepath, "filepath", filepath, "specify filepath for proxy")
+	var fpath string	//ここ少し問題
+	//引数でパスが入力されたら、fpathに代入される
+	flags.StringVar(&fpath, "fpath", fpath, "Specify the path of the file you want to target proxy on/off. プロキシのオンオフ対象のファイルのPATHを設定します")
+	var cancel bool
+	flags.BoolVar(&cancel, "cancel", false, "Cancel the registered path. 登録されているパスを解除します")
+	var check bool
+	flags.BoolVar(&check, "check", false, "Check the currently set path. 現在設定されているパスを確認します")
+	var effective bool
+	var ineffective bool
+	flags.BoolVar(&effective, "effective", false, "Uncomment the target file and activate the proxy. 対象ファイルのコメントをはずし、プロキシを有効化します")
+	flags.BoolVar(&ineffective, "ineffective", false, "Comment the target file and disable the proxy. 対象ファイルにコメントをつけ、プロキシを無効化します")
+
 	if err := flags.Parse(args[1:]); err != nil {
 		return ExitCodeParseFlagError
 	}
-	if filepath != "" {
-		fmt.Fprint(c.errStream, filepath)
+	if fpath != "" {
+		fmt.Fprintf(c.errStream, "プロキシのオンオフ対象のファイルを、%sにPATHを設定しました", fpath)
+	}
+	if cancel {
+		fmt.Fprint(c.errStream, "設定されているPATHを取り消しました")
+	}
+	if check {
+		fmt.Fprintf(c.errStream, "現在設定されているPATHは%sです", fpath)
+	}
+	if effective {
+		fmt.Fprint(c.errStream, "対象ファイルのコメントをはずし、プロキシを有効化します")
+	}
+	if ineffective {
+		fmt.Fprint(c.errStream, "対象ファイルにコメントをつけ、プロキシを無効化しました")
 	}
 	return ExitCodeOK
 }
--- cli/cli_test.go
+++ cli/cli_test.go
@@ -12,12 +12,58 @@ const FilePath = "/test/cli"
 func Test_swfpx(t *testing.T) {
 	outStream, errStream := new(bytes.Buffer), new(bytes.Buffer)
 	cli := &cli{outStream:outStream, errStream: errStream}
-	args := strings.Split("swfpx -filepath /test/cli", " ")
+	args := strings.Split("swfpx -fpath /test/cli", " ")
 	status := cli.Run(args)
 	if status != ExitCodeOK {
 		t.Errorf("ExitStatus = %d, want %d", status, ExitCodeOK)
 	}
-	expected := fmt.Sprintf(FilePath)
+	expected := fmt.Sprintf("プロキシのオンオフ対象のファイルを、%sにPATHを設定しました", FilePath)
+	//ここちょっと問題がある。fpathをグローバルにするとテストが通るが、ローカルだと通らない
+	//原因を救命する必要がある
+	if !strings.Contains(errStream.String(), expected) {
+		t.Errorf("output=%q, want %q", errStream.String(), expected)
+	}
+
+	errStream.Reset()
+	args = strings.Split("swfpx -cancel", " ")
+	status = cli.Run(args)
+	if status != ExitCodeOK {
+		t.Errorf("ExitStatus = %d, want %d", status, ExitCodeOK)
+	}
+	expected = fmt.Sprintf("設定されているPATHを取り消しました")
+	if !strings.Contains(errStream.String(), expected) {
+		t.Errorf("output=%q, want %q", errStream.String(), expected)
+	}
+
+	errStream.Reset()
+	args = strings.Split("swfpx -check", " ")
+	status = cli.Run(args)
+	if status != ExitCodeOK {
+		t.Errorf("ExitStatus = %d, want %d", status, ExitCodeOK)
+	}
+	expected = fmt.Sprintf("現在設定されているPATHは%sです", FilePath)
+	if !strings.Contains(errStream.String(), expected) {
+		t.Errorf("output=%q, want %q", errStream.String(), expected)
+	}
+
+	errStream.Reset()
+	args = strings.Split("swfpx -effective", " ")
+	status = cli.Run(args)
+	if status != ExitCodeOK {
+		t.Errorf("ExitStatus = %d, want %d", status, ExitCodeOK)
+	}
+	expected = fmt.Sprintf("対象ファイルのコメントをはずし、プロキシを有効化します")
+	if !strings.Contains(errStream.String(), expected) {
+		t.Errorf("output=%q, want %q", errStream.String(), expected)
+	}
+
+	errStream.Reset()
+	args = strings.Split("swfpx -ineffective", " ")
+	status = cli.Run(args)
+	if status != ExitCodeOK {
+		t.Errorf("ExitStatus = %d, want %d", status, ExitCodeOK)
+	}
+	expected = fmt.Sprintf("対象ファイルにコメントをつけ、プロキシを無効化しました")
 	if !strings.Contains(errStream.String(), expected) {
 		t.Errorf("output=%q, want %q", errStream.String(), expected)
 	}
