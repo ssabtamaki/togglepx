93cf80e99a6f886b866d766718a5acef82dd682e
ssab <qasw4554@gmail.com>
2019-05-05 16:55:04 +0900

    [add]#23 mainの完成
    バイナリを動かせるようにmainを完成

--- main.go
+++ main.go
@@ -1,4 +1,20 @@
 package main
 
+import (
+	"os"
+	"stepupgo/cli"
+)
+
 func main() {
+	//構造体を作る
+	p := cli.PathIPConfig{}
+	//ファイル読み込んで構造体に適用
+	err := p.ReadJsonTransfer(cli.JsonPath)
+	if err != nil {
+		os.Exit(1)
+	}
+	//cli起動、cliに構造体渡す
+	stream := cli.Stream{os.Stdout, os.Stderr}
+	os.Exit(stream.Run(os.Args, &p))
+	//終了
 }
