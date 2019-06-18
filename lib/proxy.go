0939cada21cc0cba2b81aeb679720f0b16579721
ssab <qasw4554@gmail.com>
2019-06-18 18:01:31 +0900

    fix cli
    cliに関するバグ修正

--- lib/proxy.go
+++ lib/proxy.go
@@ -47,15 +47,15 @@ func ProxyON(filename string) (err error) {
 }
 
 //ファイルのプロキシが書かれている行に#を入れたり抜いたりする
-//func ToggleProxyAuto(filename string) (err error) {
-//	input, err := ioutil.ReadFile(filename)
-//	if err != nil {
-//		return err
-//	}
-//	output := strings.NewReplacer(px, commentPx, commentPx, px).Replace(string(input))
-//	err = ioutil.WriteFile(filename, []byte(output), 0666)
-//	if err != nil {
-//		return err
-//	}
-//	return nil
-//}
+func ToggleProxyAuto(filename string) (err error) {
+	input, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return err
+	}
+	output := strings.NewReplacer(px, commentPx, commentPx, px).Replace(string(input))
+	err = ioutil.WriteFile(filename, []byte(output), 0666)
+	if err != nil {
+		return err
+	}
+	return nil
+}
